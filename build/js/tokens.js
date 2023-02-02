3f9ac8aa3182910e8032b5d728c65648811177dd
Jessica Ranft <jessica.ranft@gmail.com>
2023-02-02 17:23:30 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 02 Feb 2023 20:11:54 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#8a8a8a",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#8b5cf6"
+};
